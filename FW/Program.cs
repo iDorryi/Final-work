637e8bf995ca0ef88ce520ed39aaaabec822e56a
idorry <iannoyinbox@gmail.com>
2022-10-10 23:14:22 +0300

    Deleted secondary branch

--- FW/Program.cs
+++ FW/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Написать программу, которая из имеющегося массива строк формирует массив из строк длинна которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры или задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключетльно массивами: ");
-// Создаем и заполняем массив при помощи введения его с клавиатуры: //
-
+// 1. Создаем и заполняем стартовый массив при помощи заданных строковых значений //
 string [] StartArray = new string[4] {";/", "hello", "world","-3"};
-void PrintStartArray(string[] StartArray)
+// 2. Функция для печати массива
+void PrintArray(string[] StartArray)
 {
     int l = StartArray.Length;
     for (int i = 0; i < l; i++)
@@ -11,16 +11,15 @@ void PrintStartArray(string[] StartArray)
     }
     Console.WriteLine();
 }
-PrintStartArray(StartArray);
-
+// 3. Создаем новый массив
 string[] FinalArray = new string[StartArray.Length];
-
-void Result(string [] StartArray, string[] FinalArray)
+// 4. Создаем функцию для ввода нового массива, в котором длинна строк <= 3 символам
+void Result(string[] StartArray, string[] FinalArray)
 {
 int count = 0;
   for(int i = 0; i < StartArray.Length; i++)
  {
-  if(StartArray[i].Length <= 3) 
+  if (StartArray[i].Length <= 3) 
    {
     FinalArray[count] = StartArray[i]; 
     count++;
@@ -28,7 +27,14 @@ int count = 0;
  }
 }
 Result(StartArray, FinalArray);
-PrintStartArray(FinalArray);
+PrintArray(StartArray);
+PrintArray(FinalArray);
+
+ 
+
+
+
+
 
  
 
